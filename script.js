788635eb500fc168f3db74cd49f6a5e9b02371e1
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-04-30 12:53:35 +0200

    Feature: Formulary data validation
    Remodeled the checkbox so it now function as intended and added a onclick to the formulary button that now validates its data and gives visual feedback

--- script.js
+++ script.js
@@ -9,8 +9,51 @@ window.onscroll = (e) => {
     barFill.style.width = percentage+"%";
 }
 
-async function ReturnTop(){
-    setTimeout(function () {
+function ReturnTop(){
+    setTimeout(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' })
     }, 200);
+}
+
+function SendFormularyData(){
+    //Visual elements wich borders have to be set to red.
+    const visualElements = [document.getElementById("name"), document.getElementById("email"), document.getElementById("checkbox-visual")]
+
+    //Formulary values.
+    const name = visualElements[0].value;
+    const email = visualElements[1].value;
+    const checkbox = document.getElementById("checkbox").checked;
+    
+    //Data validation and visual feedback
+    const validData = ValidateFormularyData(name, email, checkbox, visualElements);
+
+    if(validData)
+    { console.log("Valid data"); }
+    else
+    { console.log("INVALID data"); }
+}
+
+function ValidateFormularyData(name, email, checkbox, visualElements){
+    var value = true;
+    const regrex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!(name.length >= 2 && name.length <= 100)){
+        value = false;
+        visualElements[0].style.border = "1px solid red";
+    }
+    else{ visualElements[0].style.border = null; }
+
+    if(!regrex.test(email)){
+        value = false;
+        visualElements[1].style.border = "1px solid red";
+    }
+    else{ visualElements[1].style.border = null; }
+
+    if(!checkbox){
+        value = false;
+        visualElements[2].style.border = "1px solid red";
+    }
+    else{ visualElements[2].style.border = null; }
+
+    return value;
 }
